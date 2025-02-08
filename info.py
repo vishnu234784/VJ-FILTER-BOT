2855ed7f146841b52807358b8e194e02cb0df460
vishnu234784 <vishnuku2345@gmail.com>
2025-02-09 01:35:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -84,10 +84,10 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/vj_bot_disscussion')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/vj_botz')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'vj_bot_disscussion') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/kingvj01')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/VK_Entertainment_Movie')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/VK_Entertainment_1')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/VK_Entertainment_Movie') # Support Chat Link Without https:// or @
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/VK_Entertainment_1')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
