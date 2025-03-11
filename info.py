0f8423c51854899cb74ac4ca54cbd2bbc54a1486
vishnu234784 <vishnuku2345@gmail.com>
2025-03-11 09:35:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -166,7 +166,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "http://mental-sandra-movie34-37307953.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
