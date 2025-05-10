ec0466afe791a59c551a515641f9a6317c04393a
vishnu234784 <vishnuku2345@gmail.com>
2025-05-10 22:37:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -125,9 +125,9 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
-SHORTLINK_API = environ.get('SHORTLINK_API', '')
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://shortxlinks.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '201c95000dcc7a20ad832d29e6d5aa62c70e482c')
 TUTORIAL = environ.get('TUTORIAL', '') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
